5a5204acfdecae1c45238a5aae272f0c823aa70e
Vamsi-krissh <vamsi1393@gmail.com>
2017-05-31 18:35:27 +0530

    5.3

---
+++ app1.js
@@ -0,0 +1,12 @@
+var Employee = {
+name:'Vamsi',
+age:25,
+salary:40000,
+Adress:{city:"Hyderabd",
+State :"Telangana",
+Pincode:500073
+}}
+
+
+
+console.log("Name:"+ Employee.name+"\n"+"Age:"+Employee.age+"\n"+"Salary:"+Employee.salary+"\n"+"City:"+Employee.Adress.city+"\n"+"State:"+Employee.Adress.State+"\n"+"Pincode:"+Employee.Adress.Pincode)
